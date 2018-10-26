e8c927059a2e1aa0f25e1840a48f0f7a8802a0fd
Eivind Vold Aunebakk <eivind@aunebakk.no>
2018-10-26 19:11:01 +0200

    Added namespace specifier to the component map

--- EVA/ComponentMap.hpp
+++ EVA/ComponentMap.hpp
@@ -11,11 +11,11 @@ namespace EVA
 {
 	// Macro for registering a component. Should be put inside the class declaration
 	#define REGISTER_COMPONENT_HPP(TYPE) \
-    REGISTER_CLASS_HPP(Component, TYPE)
+    REGISTER_CLASS_HPP(EVA::Component, TYPE)
 
 	// Macro for registering a component
 	#define REGISTER_COMPONENT_CPP(TYPE, NAME) \
-    REGISTER_CLASS_CPP(Component, TYPE, NAME)
+    REGISTER_CLASS_CPP(EVA::Component, TYPE, NAME)
 
 	/**
 	 * \brief Keeps track of all registerd components
